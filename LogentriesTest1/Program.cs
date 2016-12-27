d1681d76ecfd4a66aa22edbf09d24ef99f4920ad
juanonsoftware <juanonsoftware@github.com>
2016-12-27 17:12:40 +0700

    Test Logentries

---
+++ LogentriesTest1/Program.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using System;
+
+namespace LogentriesTest1
+{
+    class Program
+    {
+        private static readonly ILog LoggerLe = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            //XmlConfigurator.Configure();
+
+            var random = new Random();
+            var num1 = random.Next(1000);
+
+            LoggerLe.Info("A random number at " + DateTime.Now + " is " + num1);
+
+            Console.ReadLine();
+        }
+    }
+}
